8b8d3d3f3a0d7952654f5ce485cbbb58cacabdf4
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-12-14 21:19:58 +0530

    updated CORS

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app = FastAPI(title="FlightSense API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://flightsense-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
